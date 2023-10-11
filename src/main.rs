4e486884a45e6cb35e07ae5ecba0571f48b77bea
dalmoveras <d.verasnt@gmail.com>
2023-10-11 15:28:27 -0700

    add decryption flow

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ struct Args {
 }
 
 fn encryption() {
-     println!("Enter the plaintext:");
+    println!("Enter the plaintext:");
     let plaintext: String = text_io::read!("{}\n"); 
     println!("Enter the key(d,r) [integers and comma separated]:"); 
     let key: String = text_io::read!("{}\n"); 
@@ -35,7 +35,32 @@ fn encryption() {
     println!("{}", ciphertext);
 
 }
+
+fn decryption() {
+    println!("Enter the ciphertext:");
+    let mut ciphertext: String = text_io::read!("{}\n");
+    println!("Enter the key(d,r) [integers and comma separated]:");
+    let key: String = text_io::read!("{}\n");
+    let mut iter = key.splitn(2, ',');
+    let depth = iter.next().unwrap().parse::<usize>().unwrap();
+    let rounds = iter.next().unwrap().parse::<i32>().unwrap();
+    
+    let r = Railfence::new(depth);
+    let mut i: i32 = 0;
+    let mut plaintext = String::new();
+    ciphertext = r.decrypt(&ciphertext).unwrap();
+    
+    while i < rounds{
+        plaintext = r.decrypt(&ciphertext).unwrap();
+        ciphertext = plaintext.to_string();
+        i+=1;
+    }
+    println!("{}", plaintext);
+
+}
+
 fn main(){
     println!("[*] Railfence Cipher - Cryptography Assignment 2 [*]");
     encryption();
+    decryption();
    }
